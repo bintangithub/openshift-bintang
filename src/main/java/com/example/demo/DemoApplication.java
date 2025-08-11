e8f2b4bba324a4b9629fdf35623b5807445bcab0
bintangithub <bintangithub@gmail.com>
2025-08-11 20:33:41 +0700

    coba

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -21,7 +21,7 @@ public class DemoApplication implements CommandLineRunner {
 	public class HelloController {
 		@GetMapping("/")
 		public String hello() {
-			return "Hello, Springboot with bintang!";
+			return "Hello, Springboot with abelian!";
 		}
 	}
 
